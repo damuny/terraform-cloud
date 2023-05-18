bb907725d11c4a534e23f0389afe01123298006d
damuny <133127279+damuny@users.noreply.github.com>
2023-05-18 09:26:34 +0300

    3

--- locals.tf
+++ locals.tf
@@ -2,7 +2,7 @@ locals {
  # resource_group_name = "rg-infra-test"
   location            = "North Europe"
   virtual_network = {
-#    name          = "app-network"
+#    name          = "app-network" 
      address_space = "10.0.0.0/16"
   }
 }
