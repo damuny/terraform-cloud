d51fb60bce572f34adfd508cb7be9e5ed1a502f1
damuny <133127279+damuny@users.noreply.github.com>
2023-05-18 10:44:34 +0300

    Update main.tf
    again subnet b

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "azurerm_subnet" "subnet" {
   ]
 }
 
-resource "azurerm_subnet" "subnet" {
+resource "azurerm_subnet" "subnetb" {
   name                 = "subnet-b}"
   resource_group_name  = azurerm_resource_group.resourcegrp.name
   virtual_network_name = azurerm_virtual_network.network.name
