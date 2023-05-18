03c0fa1aea63854f3ef45f0be746e6806b274246
damuny <133127279+damuny@users.noreply.github.com>
2023-05-18 10:49:12 +0300

    Update main.tf
    corectie nume subnet

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "azurerm_subnet" "subnet" {
 }
 
 resource "azurerm_subnet" "subnetb" {
-  name                 = "subnet-b}"
+  name                 = "subnet-b"
   resource_group_name  = azurerm_resource_group.resourcegrp.name
   virtual_network_name = azurerm_virtual_network.network.name
   address_prefixes     = [cidrsubnet(local.virtual_network.address_space, 8, 1)]
